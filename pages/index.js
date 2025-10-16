e0673b59f94a7a9dc415f881c4292a4660620a08
Danuka <danukaandhimasha@gmail.com>
2025-10-17 03:11:54 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1579,7 +1579,7 @@ export default function Home() {
                   {loading && (
                     <div
                       style={{
-                        position: "absolute",
+                       
                         top: 0,
                         left: 0,
                         right: 0,

23fa15d223a3f930214087e2f33e1de09a93f14d
Danuka <danukaandhimasha@gmail.com>
2025-10-16 17:37:55 +0530

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -257,13 +257,6 @@ export default function Login() {
             Sign up
           </Link>
         </div>
-
-        <style jsx>{`
-          @keyframes spin {
-            0% { transform: rotate(0deg); }
-            100% { transform: rotate(360deg); }
-          }
-        `}</style>
       </div>
     </div>
   );

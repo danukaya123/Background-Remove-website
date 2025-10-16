93749851675194a60a1db1b815f4c463d12053ce
Danuka <danukaandhimasha@gmail.com>
2025-10-17 03:01:50 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -183,7 +183,7 @@ export default function Home() {
     setError(null);
 
     try {
-      const client = await Client.connect("Jonny001/Background-Remover-C1");
+      const client = await Client.connect("danuka21/quizontal-Background-Remover-C1");
       console.log("✅ Connected to HF Space");
 
       const result = await client.predict("/image", { image: file });

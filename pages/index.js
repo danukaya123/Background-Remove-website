a4c959f7bc5481c9bc1503e0ed015ee3945a398b
Danuka <danukaandhimasha@gmail.com>
2025-10-17 20:42:47 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -680,8 +680,8 @@ export default function Home() {
     { name: 'Upload', href: '#upload-section' },
     { name: 'Features', href: '#Features' },
     { name: 'Examples', href: '#examples' },
-    { name: 'API Documentation', href: './api' },
-    { name: 'About', href: './about' },
+    { name: 'API Documentation', href: '/api.html' },
+    { name: 'About', href: '/about.html' },
     { name: 'Blog', href: 'https://blog.quizontal.cc' }
   ].map((item) => (
     <a 
